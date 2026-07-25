12e64eccff690ec44b6a99b8f2564f7e4486e699
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-25 19:03:56 +0530

    update: routes updated to named routes

--- management/routes/web.php
+++ management/routes/web.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\StudentController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/students', [StudentController::class, 'index']);
-Route::get('/students/create', [StudentController::class, 'create']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::get('/students/{id}/edit', [StudentController::class, 'edit']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
