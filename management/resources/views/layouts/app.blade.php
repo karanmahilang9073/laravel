3d2af4a436f86f8a898d7dca9c12ff6c3eed6f6e
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-27 13:28:46 +0530

    tailwind css added in this project

--- management/resources/views/layouts/app.blade.php
+++ management/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Hub</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     @include('partials.navbar')
