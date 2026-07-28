2bb11bd5c1aa9f63074ea967a91f236e2de264da
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-28 09:55:49 +0530

    rest API created for json response

---
+++ management/app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json($students);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
