02abe6e75e026c77021ec1bebabcb57ef1840be6
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-28 10:20:24 +0530

    REST API  CRUD iplemented successfully

--- management/app/Http/Controllers/Api/StudentController.php
+++ management/app/Http/Controllers/Api/StudentController.php
@@ -16,21 +16,38 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|unique:students,email',
+            'phone' => 'required|digits:10',
+            'age' => 'required|integer|min:18|max:60',
+            'course_id' => 'required|exists:courses,id',
+        ]);
+        $student = Student::create($validate);
+        return response()->json($student, 201);
     }
 
-    public function show(string $id)
+    public function show(Student $student)
     {
-        //
+        return response()->json($student);
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, Student $student)
     {
-        //
+        $validate = $request->validate([
+            'name' => 'required|min:3|max:50',
+            'email'  => 'required|email|unique:students,email,' . $student->id,
+            'phone' => 'required|digits:10',
+            'age' => 'required|integer|min:18|max:60',
+            'course_id' => 'required|exists:courses,id'
+        ]);
+        $student->update($validate);
+        return response()->json($student);
     }
 
-    public function destroy(string $id)
+    public function destroy(Student $student)
     {
-        //
+        $student->delete();
+        return response()->json(['message' => 'student deleted successfully'], 200);
     }
 }
