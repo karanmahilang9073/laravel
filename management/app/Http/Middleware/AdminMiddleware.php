27aa4adf79f40824bc39363ca5287e56f18928b1
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-31 09:59:42 +0530

    fix: controller not found bug fixed

--- management/app/Http/Middleware/AdminMiddleware.php
+++ management/app/Http/Middleware/AdminMiddleware.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         if(!auth()->check()){
